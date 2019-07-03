13d05d34c03bbe365019a4546b90eb6330e0e567
wangyds <wangyds@yonyou.com>
2019-07-03 21:07:43 +0800

    Initial commit

---
+++ src/router/index.js
@@ -0,0 +1,45 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import About from '../views/About'
+import Home from '../views/Home'
+import News from '../views/News'
+import Message from '../views/Message'
+
+Vue.use(Router)
+
+export default new Router({
+  //创建组件路由
+  routes: [
+    {
+      path: '/',
+      redirect: '/about'
+    },
+    {
+      path: '/about',
+      name: 'About',
+      component: About
+    },
+    {
+      path: '/home',
+      name: 'Home',
+      component: Home,
+      //子路由的两种写法
+      children:[
+        {
+          //给当前路由页面设置默认值
+          path:"",
+          redirect:"/home/news"
+        },
+        {
+          path: '/home/news',
+          component: News
+        },
+        {
+          path: '/home/message',
+          component: Message
+        }
+      ]
+    }
+  ]
+})
---
+++ src/views/News.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <ul>
+            <li v-for="(news,index) in newsArr" :key="index">{{news}}</li>
+        </ul>
+    </div>
+</template>
+<script>
+    export default {
+        data(){
+            return {
+                newsArr:[
+                    'news1',
+                    'news2',
+                    'news3'
+                ]
+            }
+        }
+    }
+</script>
+<style>
+
+</style>
