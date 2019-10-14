94b3fe494045f058d397b0ca7ce2f3b6b2ab6a1e
wangyds <wangyds@yonyou.com>
2019-10-14 17:14:51 +0800

    update 主页

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,8 @@
-<style scoped>
-.layout {
-  border: 1px solid #d7dde4;
-  background: #f5f7f9;
-  position: relative;
-  border-radius: 4px;
-  overflow: hidden;
-}
-.layout-header-bar {
-  background: #fff;
-  box-shadow: 0 1px 1px rgba(0, 0, 0, 0.1);
-}
-</style>
+
 <template>
   <div class="layout">
-    <Sider :style="{position: 'fixed', height: '100vh', left: 0, overflow: 'auto'}">
-      <Menu active-name="1-2" theme="dark" width="auto" :open-names="['1']">
+    <Sider class="sider">
+      <Menu active-name="1-1" theme="dark" width="auto" :open-names="['1']">
         <Submenu name="1">
           <template slot="title">
             <Icon type="ios-navigate"></Icon>
@@ -62,3 +50,24 @@ export default {
 
 }
 </script>
+
+
+<style scoped>
+.layout {
+  border: 1px solid #d7dde4;
+  background: #f5f7f9;
+  position: relative;
+  border-radius: 4px;
+  overflow: hidden;
+}
+.layout-header-bar {
+  background: #fff;
+  box-shadow: 0 1px 1px rgba(0, 0, 0, 0.1);
+}
+.sider {
+  position: fixed;
+  height: 100%;
+  left: 0;
+  overflow: auto;
+}
+</style>
